05587898536684fb37b87023017033a44a36b931
Reinhart <reinhart377@gmail.com>
2023-06-15 19:36:26 +0700

    initial commit

--- Cloud Computing Development/deploy-flask-api/main.py
+++ Cloud Computing Development/deploy-flask-api/main.py
@@ -1,29 +1,30 @@
 import os
-
-
-
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 import keras
 import io
 import numpy as np
 from PIL import Image
+import keras.utils as image
 
 from flask import Flask, request, jsonify
+from tensorflow.keras.applications.vgg16 import VGG16
 
 # Import model
-
-
 model = keras.models.load_model("model_fc.h5")
+pre_model = VGG16(include_top=False, weights='imagenet')
 
 # Prediction function
 def predict(x):
-    predictions = model(x)
-    pred = np.argmax(predictions, axis=1)
+    img = image.load_img(x, target_size=(224,224))
+    img = image.img_to_array(img)
+    img = img.astype(np.float32) / 255
+    img = np.expand_dims(img, axis=0)
+    bt_prediction = pre_model.predict(img)
+    pred= model.predict(bt_prediction)
     return pred
 
-    # Initialize Flask server with error handling
-
-
+# Initialize Flask server with error handling
 app = Flask(__name__)
 
 
@@ -38,7 +39,9 @@ def index():
             image_bytes = file.read()
             img = Image.open(io.BytesIO(image_bytes))
             prediction = predict(img)
-            data = {"prediction": str(prediction)}
+            class_name = ['Butterflies', 'Chickens', 'Daisy', 'Dandelion', 'Duck', 'Elephants', 'Fish', 'Frog', 'Horses', 'Monkey', 'Rose', 'Snake', 'Spider', 'Squirells', 'Sunflower', 'Tulip']
+            pred = class_name[np.argmax(prediction)]
+            data = {"prediction": pred}
             return jsonify(data)
         except Exception as e:
             return jsonify({"error": str(e)})
